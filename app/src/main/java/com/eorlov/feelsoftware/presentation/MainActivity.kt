8515d07d702bb23ce28ecec3e74ba0422371a26c
Eugene <evgeniiorlov204@gmail.com>
2021-12-07 11:25:45 +0200

    Implemented base architecture

--- app/src/main/java/com/eorlov/feelsoftware/presentation/MainActivity.kt
+++ app/src/main/java/com/eorlov/feelsoftware/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.eorlov.feelsoftware
+package com.eorlov.feelsoftware.presentation
 
 import androidx.appcompat.app.AppCompatActivity
+import com.eorlov.feelsoftware.R
 
 class MainActivity : AppCompatActivity(R.layout.activity_main)
