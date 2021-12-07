279eb76b958d357a8abcae8a34fa4b25a213b99c
Eugene <evgeniiorlov204@gmail.com>
2021-12-07 11:47:29 +0200

    Implemented base class for fragments

---
+++ app/src/main/java/com/eorlov/feelsoftware/presentation/base/BaseFragment.kt
@@ -0,0 +1,51 @@
+package com.eorlov.feelsoftware.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VBinding : ViewBinding>(
+    private val inflaterMethod: (LayoutInflater, ViewGroup?, Boolean) -> VBinding
+) :
+    Fragment() {
+
+    private var _binding: VBinding? = null
+    val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = inflaterMethod.invoke(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+        setObservers()
+        initialize()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    protected open fun setListeners() {}
+    protected open fun setObservers() {}
+    protected open fun initialize() {}
+
+    protected open fun lockUI() {
+        activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+
+    protected open fun unlockUI() {
+        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+}
