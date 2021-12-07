09b14737d31bcf6b16189a4f77d2b6dc8816e9f7
Eugene <evgeniiorlov204@gmail.com>
2021-12-07 11:40:34 +0200

    Added Hilt annotation

--- app/src/main/java/com/eorlov/feelsoftware/presentation/App.kt
+++ app/src/main/java/com/eorlov/feelsoftware/presentation/App.kt
@@ -1,6 +1,7 @@
 package com.eorlov.feelsoftware.presentation
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-class App : Application() {
-}
+@HiltAndroidApp
+class App : Application()
